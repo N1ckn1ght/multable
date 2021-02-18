cb7baf93942a3b908b8a557cc3a57bd5fc878d31
N1ckn1ght <50321432+N1ckn1ght@users.noreply.github.com>
2021-02-18 16:45:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,24 +7,31 @@
 <body>
 	<?php
 		echo '<table border=1px>';
-		for ($i = 0; $i <= 10; $i++) {
+		for ($i = 0; $i <= 10; $i++) 
+		{
 			echo '<tr>';
-			for ($j = 0; $j <= 10; $j++) {
-        if ($i == 0 && $j == 0) {
+			for ($j = 0; $j <= 10; $j++) 
+			{
+				if ($i == 0 && $j == 0) 
+				{
 					echo '<td width=25 height=25 align=center bgcolor=800000</td>';
-        }
-        else if ($i == 0) {
-          echo '<td width=25 height=25 align=center bgcolor=FFCB9C>'.$j.'</td>';
-        }
-        else if ($j == 0) {
-          echo '<td width=25 height=25 align=center bgcolor=FFCB9C>'.$i.'</td>';
-        }
-        else if ($i == $j) {
-          echo '<td width=25 height=25 align=center bgcolor=F6FC92>'.$i * $j.'</td>';
-        }
-        else {
-          echo '<td width=25 height=25 align=center>'.$i * $j.'</td>';
-        }
+				}
+				else if ($i == 0) 
+				{
+					echo '<td width=25 height=25 align=center bgcolor=FFCB9C>'.$j.'</td>';
+				}
+				else if ($j == 0) 
+				{
+					echo '<td width=25 height=25 align=center bgcolor=FFCB9C>'.$i.'</td>';
+				}
+				else if ($i == $j) 
+				{
+					echo '<td width=25 height=25 align=center bgcolor=F6FC92>'.$i * $j.'</td>';
+				}
+				else 
+				{
+					echo '<td width=25 height=25 align=center>'.$i * $j.'</td>';
+				}
 			}
 			echo '</tr>';
 		}
